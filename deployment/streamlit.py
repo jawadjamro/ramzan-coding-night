19a4e72b6b38029937efa4648b3f7cb14d4face7
Jawad Hassan <159148373+jawadjamro@users.noreply.github.com>
2025-03-15 09:41:29 +0500

    Add files via upload

---
+++ deployment/streamlit.py
@@ -0,0 +1,8 @@
+#import streamlit as st
+
+#st.title("Simple Streamlit App")  # Corrected typo here
+
+#user_input = st.text_input("Enter Your message")  # Fixed spelling in the prompt
+
+#if st.button("Show text"):  # Corrected typo here
+#    st.write(f"You entered: {user_input}")
